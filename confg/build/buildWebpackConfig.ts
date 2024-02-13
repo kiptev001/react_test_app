5685078e41fc19f00c5d6e9d1f7f7e24e0120a77
kiptev001 <kiptev@evrone.com>
2024-02-13 12:08:09 +0300

    initial

---
+++ confg/build/buildWebpackConfig.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { IBuildOptions } from './types/config';
+import webpack from 'webpack';
+import { buildDevServer } from './buildDevServer';
+
+export function buildWebpackConfig(
+  options: IBuildOptions
+): webpack.Configuration {
+  const { path, mode, isDev } = options;
+  return {
+    mode: mode,
+    entry: path.entry,
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.build,
+      clean: true,
+    },
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+  };
+}
---
+++ confg/build/types/config.ts
@@ -0,0 +1,19 @@
+export type BuildMode = 'production' | 'development';
+
+export interface IPath {
+  entry: string;
+  build: string;
+  html: string;
+}
+
+export interface IBuildOptions {
+  mode: BuildMode;
+  path: IPath;
+  PORT: number;
+  isDev: boolean;
+}
+
+export interface EnvBuild {
+  port: number;
+  mode: BuildMode;
+}
