ba679caf571e4d4519f0563d3ca1215808c88d3c
Nikita Kiptev <nikitakiptev@MacBook-Pro-Nikita.local>
2024-03-01 16:55:33 +0300

    fix router for test

--- src/shared/lib/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/renderWithTranslation/renderWithTranslation.tsx
@@ -2,11 +2,23 @@ import { render } from '@testing-library/react'
 import type { ReactNode } from 'react'
 import React from 'react'
 import { I18nextProvider } from 'react-i18next'
+import { MemoryRouter } from 'react-router-dom'
 import i18nTest from 'shared/config/i18n/i18nTest'
 
-export const renderWithTranslation = (component: ReactNode) => {
+export interface RenderWithTranslationOptions {
+  readonly route?: string
+}
+
+export const renderWithTranslation = (
+  component: ReactNode, options: RenderWithTranslationOptions = {}
+) => {
+  const { route = '/' } = options;
+
   return render(
-    <I18nextProvider i18n={i18nTest}>
-      {component}
-    </I18nextProvider>)
+    <MemoryRouter initialEntries={[route]}>
+      <I18nextProvider i18n={i18nTest}>
+        {component}
+      </I18nextProvider>
+    </MemoryRouter>
+  )
 }
