3abde2ef447da46bc0763ad1bae785a55b58cddd
Nikita Kiptev <kiptev@evrone.com>
2024-05-03 14:41:27 +0700

    add JSON-SERVER

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,7 +3,7 @@ import { type Configuration as DevServerConfiguration } from 'webpack-dev-server
 
 export function buildDevServer(options: IBuildOptions): DevServerConfiguration {
   return {
-    port: options.PORT | 3000,
+    port: 3000,
     open: true,
     historyApiFallback: true,
     hot: true
